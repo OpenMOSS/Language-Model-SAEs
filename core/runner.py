40badfdd43b9b27f583ee743d6c3ff5e6af8dadf
Dest1n1 <dest1nis2022@gmail.com>
2024-03-12 16:36:10 +0800

    feat: add eval runner

--- core/runner.py
+++ core/runner.py
@@ -8,7 +8,8 @@ from transformers import AutoModelForCausalLM
 
 from transformer_lens import HookedTransformer
 
-from core.config import ActivationGenerationConfig, LanguageModelSAEAnalysisConfig, LanguageModelSAETrainingConfig
+from core.config import ActivationGenerationConfig, LanguageModelSAEAnalysisConfig, LanguageModelSAETrainingConfig, LanguageModelSAEConfig
+from core.evals import run_evals
 from core.sae import SparseAutoEncoder
 from core.activation.activation_dataset import make_activation_dataset
 from core.activation.activation_store import ActivationStore
@@ -33,6 +34,32 @@ def language_model_sae_runner(cfg: LanguageModelSAETrainingConfig):
         sae,
         activation_store,
         cfg,
+    )
+
+    if cfg.log_to_wandb and (not cfg.use_ddp or cfg.rank == 0):
+        wandb.finish()
+
+    return sae
+
+def language_model_sae_eval_runner(cfg: LanguageModelSAEConfig):
+    sae = SparseAutoEncoder(cfg=cfg)
+    if cfg.from_pretrained_path is not None:
+        sae.load_state_dict(torch.load(cfg.from_pretrained_path, map_location=cfg.device)["sae"])
+    hf_model = AutoModelForCausalLM.from_pretrained('gpt2', cache_dir=cfg.cache_dir, local_files_only=cfg.local_files_only)
+    model = HookedTransformer.from_pretrained('gpt2', device=cfg.device, cache_dir=cfg.cache_dir, hf_model=hf_model)
+    model.eval()
+    activation_store = ActivationStore.from_config(model=model, cfg=cfg)
+        
+    if cfg.log_to_wandb and (not cfg.use_ddp or cfg.rank == 0):
+        wandb.init(project=cfg.wandb_project, config=cast(Any, cfg), name=cfg.run_name, entity=cfg.wandb_entity)
+
+    # train SAE
+    sae = run_evals(
+        model,
+        sae,
+        activation_store,
+        cfg,
+        0
     )
 
     if cfg.log_to_wandb and (not cfg.use_ddp or cfg.rank == 0):
