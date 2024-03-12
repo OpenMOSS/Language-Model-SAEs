fix(eval): ev computation

core/evals.py
@@ -64,8 +64,10 @@ def run_evals(
     l2_norm_ratio = l2_norm_out / l2_norm_in
 
     pseudo_x_hat = aux["x_hat"] / l2_norm_out.unsqueeze(-1) * l2_norm_in.unsqueeze(-1)
-    explained_variance = 1 - (pseudo_x_hat - original_act).pow(2).sum(dim=-1) / (original_act - original_act.mean(dim=0, keepdim=True)).pow(2).sum(dim=-1)
+    explained_variance = 1 - (pseudo_x_hat - original_act).pow(2).sum(dim=-1) / (original_act - original_act.mean(dim=0, keepdim=True).mean(dim=1, keepdim=True)).pow(2).sum(dim=-1)
     l0 = (aux["feature_acts"] > 0).float().sum(-1)
+
+    # TODO: DDP
 
     metrics = {
         # l2 norms