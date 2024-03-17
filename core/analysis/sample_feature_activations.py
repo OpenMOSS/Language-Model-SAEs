b7d2457c5d66a3a3f6ba7566a723aa7cde57d3f3
Dest1n1 <dest1nis2022@gmail.com>
2024-03-17 14:44:05 +0800

    feat: save feature activations as huggingface dataset

--- core/analysis/sample_feature_activations.py
+++ core/analysis/sample_feature_activations.py
@@ -23,6 +23,9 @@ def sample_feature_activations(
     activation_store: ActivationStore,
     cfg: LanguageModelSAEAnalysisConfig,
 ):
+    if cfg.use_ddp:
+        print_once("Not supported yet")
+
     total_analyzing_tokens = cfg.total_analyzing_tokens
     total_analyzing_steps = total_analyzing_tokens // cfg.store_batch_size // cfg.context_size
 
@@ -32,13 +35,9 @@ def sample_feature_activations(
     n_training_steps = 0
     n_training_tokens = 0
 
-    if cfg.use_ddp:
-        sae = DDP(sae, device_ids=[cfg.rank], output_device=cfg.device)
-
     sae.eval()
 
-    if not cfg.use_ddp or cfg.rank == 0:
-        pbar = tqdm(total=total_analyzing_tokens, desc="Sampling activations", smoothing=0.01)
+    pbar = tqdm(total=total_analyzing_tokens, desc="Sampling activations", smoothing=0.01)
 
     sample_result = {
         "weights": torch.empty((0, cfg.d_sae), dtype=cfg.dtype, device=cfg.device),
@@ -47,8 +46,8 @@ def sample_feature_activations(
         "contexts": torch.empty((0, cfg.d_sae, cfg.context_size), dtype=torch.long, device=cfg.device),
     }
     act_times = torch.zeros((cfg.d_sae,), dtype=torch.long, device=cfg.device)
-    feature_act_bins = torch.arange(0, (cfg.n_bins + 1) * cfg.bin_width, cfg.bin_width, device=cfg.device, dtype=cfg.dtype)
-    feature_act_hist = torch.zeros((cfg.d_sae, cfg.n_bins), dtype=torch.long, device=cfg.device)
+    feature_acts_all = [torch.empty((0,), dtype=cfg.dtype, device=cfg.device) for _ in range(cfg.d_sae)]
+    max_feature_acts = torch.zeros((cfg.d_sae,), dtype=cfg.dtype, device=cfg.device)
 
     sort_key = "elt" if cfg.enable_sampling else "weights"
 
@@ -85,58 +84,33 @@ def sample_feature_activations(
         # Sort elt, and extract the top n_samples
         sample_result = sort_dict_of_tensor(sample_result, sort_dim=0, sort_key=sort_key, descending=True)
         sample_result = {k: v[:cfg.n_samples] for k, v in sample_result.items()}
+        
 
-        # Update feature activation histogram
-        feature_act_hist += torch.histc(
-            aux_data["feature_acts"].clamp(min=0.0).flatten(),
-            bins=cfg.n_bins,
-            min=0.0,
-            max=(cfg.n_bins + 1) * cfg.bin_width,
-        ).view(cfg.d_sae, cfg.n_bins)
+        # Update feature activation histogram every 10 steps
+        if n_training_steps % 10 == 0:
+            feature_acts_cur = rearrange(aux_data["feature_acts"], 'batch_size context_size d_sae -> d_sae (batch_size context_size)')
+            for i in range(cfg.d_sae):
+                feature_acts_all[i] = torch.cat([feature_acts_all[i], feature_acts_cur[i][feature_acts_cur[i] > 0.0]], dim=0)
+
+        max_feature_acts = torch.max(max_feature_acts, aux_data["feature_acts"].max(dim=0).values.max(dim=0).values)
 
         n_tokens_current = torch.tensor(batch.size(0) * batch.size(1), device=cfg.device, dtype=torch.int)
-        if cfg.use_ddp:
-            dist.reduce(n_tokens_current, dst=0)
         n_training_tokens += n_tokens_current.item()
-
         n_training_steps += 1
 
-        if not cfg.use_ddp or cfg.rank == 0:
-            pbar.update(n_tokens_current.item())
+        pbar.update(n_tokens_current.item())
 
-    if not cfg.use_ddp or cfg.rank == 0:
-        pbar.close()
+    pbar.close()
 
-    if cfg.use_ddp:
-        print_once("Gathering top feature activations")
+    sample_result = {
+        k: rearrange(v, 'n_samples d_sae ... -> d_sae n_samples ...') for k, v in sample_result.items()
+    }
 
-        if cfg.rank == 0:
-            all_result = {
-                k: [torch.empty_like(v) for _ in range(cfg.world_size)] for k, v in sample_result.items()
-            }
+    result = {
+        "act_times": act_times,
+        "feature_acts_all": feature_acts_all,
+        "max_feature_acts": max_feature_acts,
+        **sample_result,
+    }
 
-        for k in sample_result:
-            dist.gather(sample_result[k], all_result[k], dst=0)
-
-        dist.reduce(act_times, dst=0)
-        dist.reduce(feature_act_hist, dst=0)
-
-        if cfg.rank == 0:
-            sample_result = {
-                k: torch.cat(v, dim=0) for k, v in all_result.items()
-            }
-            sample_result = sort_dict_of_tensor(sample_result, sort_dim=0, sort_key=sort_key, descending=True)
-
-    if not cfg.use_ddp or cfg.rank == 0:
-        sample_result = {
-            k: rearrange(v, 'n_samples d_sae ... -> d_sae n_samples ...') for k, v in sample_result.items()
-        }
-
-        result = {
-            "act_times": act_times,
-            "feature_act_bins": feature_act_bins,
-            "feature_act_hist": feature_act_hist,
-            **sample_result,
-        }
-
-        Dataset.from_dict(result).save_to_disk(cfg.analysis_save_path, num_shards=1024)
+    Dataset.from_dict(result).save_to_disk(cfg.analysis_save_path, num_shards=1024)
