99407855cde9be560542b3c2d8299db6f3a75c5d
Dest1n1 <dest1nis2022@gmail.com>
2024-03-18 23:37:34 +0800

    fix: pack tokens which cannot be directly decoded

--- server/app.py
+++ server/app.py
@@ -1,14 +1,19 @@
-import torch
 import uvicorn
-from transformer_lens import HookedTransformer
+from transformers import GPT2Tokenizer
 
-from fastapi import FastAPI
 from datasets import Dataset
+
+import msgpack
+
+from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 
 app = FastAPI()
 
-model = HookedTransformer.from_pretrained('gpt2', device="cuda")
-model.eval()
+app.add_middleware(GZipMiddleware, minimum_size=1000)
+
+tokenizer: GPT2Tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 
 feature_activation_cache = {}
 
@@ -23,17 +28,26 @@ def feature_info(feature_index: int):
     n_samples = len(feature_activation["feature_acts"])
     samples = [
         {
-            "context": model.to_str_tokens(torch.tensor(feature_activation["contexts"][i], device="cuda")),
+            "context": [bytearray([tokenizer.byte_decoder[c] for c in t]) for t in tokenizer.convert_ids_to_tokens(feature_activation["contexts"][i])],
             "feature_acts": feature_activation["feature_acts"][i],
         }
         for i in range(n_samples)
     ]
-    return {
+    return Response(content=msgpack.packb({
         "feature_index": feature_index,
         "act_times": feature_activation["act_times"],
         "max_feature_act": feature_activation["max_feature_acts"],
         "samples": samples,
-    }
+    }), media_type="application/x-msgpack")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 if __name__ == "__main__":
     uvicorn.run("app:app", port=5432, log_level="info")
