86c9bdee8381a438a11fc1d5856c01ff5bd27cfb
Dest1n1 <dest1nis2022@gmail.com>
2024-03-19 16:19:03 +0800

    chore: remove vite proxy

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -5,32 +5,6 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api/": {
-        target: "http://localhost:5432/",
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-        configure: (proxy, _options) => {
-          proxy.on("error", (err, _req, _res) => {
-            console.log("proxy error", err);
-          });
-          proxy.on("proxyReq", (_, req, _res) => {
-            console.log("Sending Request to the Target:", req.method, req.url);
-          });
-          proxy.on("proxyRes", (proxyRes, req, _res) => {
-            console.log(
-              "Received Response from the Target:",
-              proxyRes.statusCode,
-              req.url
-            );
-          });
-        },
-      },
-    },
-    host: "localhost",
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
