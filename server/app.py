9d48bbede1a5b54cb1d10891b87a3264ea11e322
Dest1n1 <dest1nis2022@gmail.com>
2024-03-28 19:08:02 +0800

    refactor: migrate result to MongoDB

--- server/app.py
+++ server/app.py
@@ -18,9 +18,12 @@ from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
 
+import plotly.express as px
+
 from core.config import SAEConfig
 from core.sae import SparseAutoEncoder
-import plotly.express as px
+import server.database as db
+
 
 result_dir = os.environ.get("RESULT_DIR", "results")
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -35,18 +38,7 @@ hf_model = AutoModelForCausalLM.from_pretrained('gpt2')
 model = HookedTransformer.from_pretrained('gpt2', device=device, hf_model=hf_model)
 model.eval()
 
-feature_activation_cache = {}
 sae_cache = {}
-
-def get_feature_activation(dictionary_name: str) -> Dict[str, Dataset]:
-    analysis_names = os.listdir(os.path.join(result_dir, dictionary_name, "analysis"))
-    if dictionary_name not in feature_activation_cache:
-        feature_activation_cache[dictionary_name] = {}
-    for analysis_name in analysis_names:
-        if os.path.isdir(os.path.join(result_dir, dictionary_name, "analysis", analysis_name)) and os.path.exists(os.path.join(result_dir, dictionary_name, "analysis", analysis_name, "state.json")):
-            if analysis_name not in feature_activation_cache[dictionary_name]:
-                feature_activation_cache[dictionary_name][analysis_name] = Dataset.load_from_disk(os.path.join(result_dir, dictionary_name, "analysis", analysis_name))
-    return feature_activation_cache[dictionary_name]
 
 def get_sae(dictionary_name: str) -> SparseAutoEncoder:
     if dictionary_name not in sae_cache:
@@ -80,73 +72,47 @@ def make_serializable(obj):
 
 @app.get("/dictionaries")
 def list_dictionaries():
-    dictionaries = os.listdir(result_dir)
-    return [d for d in dictionaries if os.path.isdir(os.path.join(result_dir, d)) and os.path.exists(os.path.join(result_dir, d, "analysis"))]
-
-@app.post("/dictionaries/{dictionary_name}/features/load")
-def load_feature_activations(dictionary_name: str):
-    try:
-        feature_activations = get_feature_activation(dictionary_name)
-    except FileNotFoundError:
-        return Response(content=f"Dictionary {dictionary_name} not found", status_code=404)
-    
-    if "top_activations" not in feature_activations:
-        return Response(content=f"Dictionary {dictionary_name} does not have top feature activations", status_code=404)
+    return db.list_dictionaries()
 
 @app.get("/dictionaries/{dictionary_name}/features/{feature_index}")
 def feature_info(dictionary_name: str, feature_index: str):
-    try:
-        feature_activations = get_feature_activation(dictionary_name)
-    except FileNotFoundError:
-        return Response(content=f"Dictionary {dictionary_name} not found", status_code=404)
-    
-    if "top_activations" not in feature_activations:
-        return Response(content=f"Dictionary {dictionary_name} does not have top feature activations", status_code=404)
-    
     if isinstance(feature_index, str):
         if feature_index == "random":
-            nonzero_feature_indices = torch.tensor(feature_activations["top_activations"]["max_feature_acts"]).nonzero(as_tuple=True)[0]
-            feature_index = nonzero_feature_indices[torch.randint(len(nonzero_feature_indices), (1,))].item()
+            feature = db.get_random_alive_feature(dictionary_name)
         else:
             try:
                 feature_index = int(feature_index)
             except ValueError:
                 return Response(content=f"Feature index {feature_index} is not a valid integer", status_code=400)
-        
-    if feature_index < 0 or feature_index >= len(feature_activations["top_activations"]):
-        return Response(content=f"Feature index {feature_index} is out of range", status_code=400)
+            feature = db.get_feature(dictionary_name, feature_index)
+
+    if feature is None:
+        return Response(content=f"Feature {feature_index} not found in dictionary {dictionary_name}", status_code=404)
     
     sample_groups = []
-    for analysis_name, dataset in feature_activations.items():
-        feature_activation = dataset[feature_index]
+    for analysis in feature["analysis"]:
         samples = [
             {
-                "context": [bytearray([tokenizer.byte_decoder[c] for c in t]) for t in tokenizer.convert_ids_to_tokens(feature_activation["contexts"][i])],
-                "feature_acts": feature_activation["feature_acts"][i],
+                "context": [bytearray([tokenizer.byte_decoder[c] for c in t]) for t in tokenizer.convert_ids_to_tokens(analysis["contexts"][i])],
+                "feature_acts": analysis["feature_acts"][i],
             }
-            for i in range(len(feature_activation["feature_acts"]))
+            for i in range(len(analysis["feature_acts"]))
         ]
         sample_groups.append({
-            "analysis_name": analysis_name,
+            "analysis_name": analysis["name"],
             "samples": samples,
         })
     
-    # Sort results so that top activations are first
-    sample_groups.sort(key=cmp_to_key(lambda a, b: -1 if a["analysis_name"] == "top_activations" else 1 if b["analysis_name"] == "top_activations" else 0))
-
-    fig = px.histogram(feature_activation["feature_acts_all"], width=600, nbins=50)
-    # fig.update_xaxes(title_text="Feature Activation Level")
-    # fig.update_yaxes(title_text="Count")
-    # fig.update_layout(showlegend=False)
+    fig = px.histogram(feature["feature_acts_all"], width=600, nbins=50)
     
-    return Response(content=msgpack.packb({
-        "feature_index": feature_index,
+    return Response(content=msgpack.packb(make_serializable({
+        "feature_index": feature["index"],
         "dictionary_name": dictionary_name,
-        "feature_activation_histogram": make_serializable(fig.to_dict()['data']),
-        "act_times": feature_activations["top_activations"]["act_times"][feature_index],
-        "max_feature_act": feature_activations["top_activations"]["max_feature_acts"][feature_index],
+        "feature_activation_histogram": fig.to_dict()['data'],
+        "act_times": feature["act_times"],
+        "max_feature_act": feature["max_feature_acts"],
         "sample_groups": sample_groups,
-    }), media_type="application/x-msgpack")
+    })), media_type="application/x-msgpack")
 
 @app.post("/dictionaries/{dictionary_name}/features/{feature_index}/custom")
 def feature_activation_custom_input(dictionary_name: str, feature_index: int, input_text: str):
