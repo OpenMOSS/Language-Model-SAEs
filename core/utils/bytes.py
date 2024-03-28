c847119970f37883537f35327d4f40d9109dcb3b
Dest1n1 <dest1nis2022@gmail.com>
2024-03-28 19:08:02 +0800

    refactor: migrate result to MongoDB

---
+++ core/utils/bytes.py
@@ -0,0 +1,11 @@
+import io
+import numpy as np
+
+def np_to_bytes(arr):
+    with io.BytesIO() as buffer:
+        np.save(buffer, arr)
+        return buffer.getvalue()
+
+def bytes_to_np(b):
+    with io.BytesIO(b) as buffer:
+        return np.load(buffer)
