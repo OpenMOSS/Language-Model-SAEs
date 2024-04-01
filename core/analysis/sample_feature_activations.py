e3d37d9393ca8386623734ede5e6902512e85165
Dest1n1 <dest1nis2022@gmail.com>
2024-04-01 19:43:56 +0800

    refactor(analysis): analysis to database

--- core/analysis/sample_feature_activations.py
+++ core/analysis/sample_feature_activations.py
@@ -39,13 +39,13 @@ def sample_feature_activations(
 
     pbar = tqdm(total=total_analyzing_tokens, desc="Sampling activations", smoothing=0.01)
 
-    sample_result = {
+    sample_result = {k: {
         "elt": torch.empty((0, cfg.d_sae), dtype=cfg.dtype, device=cfg.device),
         "feature_acts": torch.empty((0, cfg.d_sae, cfg.context_size), dtype=cfg.dtype, device=cfg.device),
         "contexts": torch.empty((0, cfg.d_sae, cfg.context_size), dtype=torch.long, device=cfg.device),
-    }
+    } for k in cfg.subsample.keys()}
     act_times = torch.zeros((cfg.d_sae,), dtype=torch.long, device=cfg.device)
-    feature_acts_all = [torch.empty((0,), dtype=cfg.dtype, device=cfg.device) for _ in range(cfg.d_sae)] if cfg.subsample is None else None
+    feature_acts_all = [torch.empty((0,), dtype=cfg.dtype, device=cfg.device) for _ in range(cfg.d_sae)]
     max_feature_acts = torch.zeros((cfg.d_sae,), dtype=cfg.dtype, device=cfg.device)
 
     while n_training_tokens < total_analyzing_tokens:
@@ -73,7 +73,7 @@ def sample_feature_activations(
         if cfg.subsample is not None:
             elt[aux_data["feature_acts"].max(dim=1).values >= max_feature_acts.unsqueeze(0) * cfg.subsample] = -torch.inf
             
-        sample_result = concat_dict_of_tensor(
+        sample_result = {k: concat_dict_of_tensor(
             sample_result,
             {
                 "elt": elt,
@@ -81,15 +81,17 @@ def sample_feature_activations(
                 "contexts": repeat(batch, 'batch_size context_size -> batch_size d_sae context_size', d_sae=cfg.d_sae),
             },
             dim=0,
-        )
+        ) for k in cfg.subsample.keys()}
 
         # Sort elt, and extract the top n_samples
-        sample_result = sort_dict_of_tensor(sample_result, sort_dim=0, sort_key="elt", descending=True)
-        sample_result = {k: v[:cfg.n_samples] for k, v in sample_result.items()}
+        sample_result = {k: sort_dict_of_tensor(sample_result, sort_dim=0, sort_key="elt", descending=True) for k in cfg.subsample.keys()}
+        sample_result = {k1: {
+            k2: v2[:cfg.subsample[k1]["n_samples"]] for k2, v2 in v1.items()
+        } for k1, v1 in sample_result.items()}
         
 
         # Update feature activation histogram every 10 steps
-        if cfg.subsample is None and n_training_steps % 10 == 0:
+        if n_training_steps % 10 == 0:
             feature_acts_cur = rearrange(aux_data["feature_acts"], 'batch_size context_size d_sae -> d_sae (batch_size context_size)')
             for i in range(cfg.d_sae):
                 feature_acts_all[i] = torch.cat([feature_acts_all[i], feature_acts_cur[i][feature_acts_cur[i] > 0.0]], dim=0)
@@ -104,18 +106,22 @@ def sample_feature_activations(
 
     pbar.close()
 
-    sample_result = {
-        k: rearrange(v, 'n_samples d_sae ... -> d_sae n_samples ...') for k, v in sample_result.items()
-    }
+    sample_result = {k1: {
+        k2: rearrange(v2, 'n_samples d_sae ... -> d_sae n_samples ...') for k2, v2 in v1.items()
+    } for k1, v1 in sample_result.items()}
 
     result = {
         "index": torch.arange(cfg.d_sae, device=cfg.device, dtype=torch.long),
         "act_times": act_times,
         "feature_acts_all": feature_acts_all,
         "max_feature_acts": max_feature_acts,
-        **sample_result,
+        "analysis": [
+            {
+                "name": k,
+                "feature_acts": v["feature_acts"],
+                "contexts": v["contexts"],
+            } for k, v in sample_result.items()
+        ],
     }
 
-    path = os.path.join(cfg.exp_result_dir, cfg.exp_name, "analysis", cfg.analysis_name)
-
-    Dataset.from_dict(result).save_to_disk(path, num_shards=1024)
+    return result
--- core/test.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-x = torch.zeros([3,4])
-print(x.shape)
-x = x.unsqueeze(0)
-print(x.shape)
