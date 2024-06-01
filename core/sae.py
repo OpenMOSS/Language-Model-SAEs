99f8bf27f356e9784e7d5c1657e96ec26df60236
Hzfinfdu <zfhe00@gmail.com>
2024-06-01 14:27:48 +0000

    fix(sae): bring back decoder bias

--- core/sae.py
+++ core/sae.py
@@ -45,6 +45,7 @@ class SparseAutoEncoder(HookedRootModule):
         self.decoder = torch.nn.Parameter(torch.empty((cfg.d_sae, cfg.d_model), dtype=cfg.dtype, device=cfg.device))
         torch.nn.init.kaiming_uniform_(self.decoder)
         self.set_decoder_norm_to_unit_norm()
+        self.decoder_bias = torch.nn.Parameter(torch.empty((cfg.d_model,), dtype=cfg.dtype, device=cfg.device))
 
         self.encoder_bias = torch.nn.Parameter(torch.empty((cfg.d_sae,), dtype=cfg.dtype, device=cfg.device))
         torch.nn.init.zeros_(self.encoder_bias)
@@ -66,6 +67,8 @@ class SparseAutoEncoder(HookedRootModule):
         ]
         if self.cfg.use_glu_encoder:
             base_parameters.extend([self.encoder_glu, self.encoder_bias_glu])
+        if self.cfg.use_decoder_bias:
+            base_parameters.append(self.decoder_bias)
         for p in self.parameters():
             p.requires_grad_(False)
         for p in base_parameters:
@@ -79,6 +82,8 @@ class SparseAutoEncoder(HookedRootModule):
             self.feature_act_scale,
             self.decoder,
         ]
+        if self.cfg.use_decoder_bias:
+            finetune_for_suppression_parameters.append(self.decoder_bias)
         for p in self.parameters():
             p.requires_grad_(False)
         for p in finetune_for_suppression_parameters:
@@ -134,6 +139,9 @@ class SparseAutoEncoder(HookedRootModule):
         if label is None:
             label = x
 
+        if self.cfg.use_decoder_bias and self.cfg.apply_decoder_bias_to_pre_encoder:
+            x = x - self.decoder_bias
+
         x = x * self.compute_norm_factor(x)
 
         hidden_pre = einsum(
@@ -179,6 +187,9 @@ class SparseAutoEncoder(HookedRootModule):
             self.decoder,
             "... d_sae, d_sae d_model -> ... d_model",
         )
+        if self.cfg.use_decoder_bias:
+            reconstructed = reconstructed + self.decoder_bias
+
         reconstructed = self.hook_reconstructed(reconstructed)
 
         return reconstructed
