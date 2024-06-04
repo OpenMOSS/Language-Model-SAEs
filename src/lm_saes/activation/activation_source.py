2728c5cbce858189bca15a1a4cf6be94f26fc36c
Hzfinfdu <zfhe00@gmail.com>
2024-06-04 08:54:14 +0000

    fix(activation): remove context info in activation_source

--- src/lm_saes/activation/activation_source.py
+++ src/lm_saes/activation/activation_source.py
@@ -47,14 +47,7 @@ class TokenActivationSource(ActivationSource):
         with torch.no_grad():
             _, cache = self.model.run_with_cache(tokens, names_filter=self.cfg.hook_points)
 
-            batch_size = tokens.size(0)
-            seq_len = tokens.size(1)
-
-            ret = {
-                "position": repeat(torch.arange(seq_len, device=self.cfg.device, dtype=torch.long), 'l -> (b l)', b=batch_size),
-                "context": repeat(tokens.to(dtype=torch.long), 'b l -> (b repeat) l', repeat=seq_len),
-                **{k: rearrange(cache[k].to(dtype=self.cfg.dtype, device=self.cfg.device), "b l d -> (b l) d") for k in self.cfg.hook_points},
-            }
+            ret = {k: rearrange(cache[k].to(dtype=self.cfg.dtype, device=self.cfg.device), "b l d -> (b l) d") for k in self.cfg.hook_points}
 
             return ret
     
