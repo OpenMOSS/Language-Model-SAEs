b903a2b6e921ce11f96cd7c42d3aeffb480f15cd
Hzfinfdu <zfhe00@gmail.com>
2024-06-13 12:03:28 +0000

    fix(frontend): implement a byte-to-unicode function instead of using the hf implementation in tokenizer

--- src/lm_saes/utils/bytes.py
+++ src/lm_saes/utils/bytes.py
@@ -1,5 +1,7 @@
 import io
 import numpy as np
+from functools import lru_cache
+
 
 def np_to_bytes(arr):
     with io.BytesIO() as buffer:
@@ -9,3 +11,28 @@ def np_to_bytes(arr):
 def bytes_to_np(b):
     with io.BytesIO(b) as buffer:
         return np.load(buffer)
+
+
+@lru_cache()
+def bytes_to_unicode():
+    """
+    Returns list of utf-8 byte and a mapping to unicode strings. We specifically avoids mapping to whitespace/control
+    characters the bpe code barfs on.
+
+    The reversible bpe codes work on unicode strings. This means you need a large # of unicode characters in your vocab
+    if you want to avoid UNKs. When you're at something like a 10B token dataset you end up needing around 5K for
+    decent coverage. This is a significant percentage of your normal, say, 32K bpe vocab. To avoid that, we want lookup
+    tables between utf-8 bytes and unicode strings.
+    """
+    bs = (
+        list(range(ord("!"), ord("~") + 1)) + list(range(ord("¡"), ord("¬") + 1)) + list(range(ord("®"), ord("ÿ") + 1))
+    )
+    cs = bs[:]
+    n = 0
+    for b in range(2**8):
+        if b not in bs:
+            bs.append(b)
+            cs.append(2**8 + n)
+            n += 1
+    cs = [chr(n) for n in cs]
+    return dict(zip(bs, cs))
