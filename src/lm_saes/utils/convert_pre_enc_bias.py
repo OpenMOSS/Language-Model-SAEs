81deefbc3b2c4b08d4fb5e2eebc84578c017ec40
Hzfinfdu <zfhe19@fudan.edu.cn>
2024-07-31 12:18:42 +0800

    feat(sae): add a utils func to merge pre-enc bias into enc bias

---
+++ src/lm_saes/utils/convert_pre_enc_bias.py
@@ -0,0 +1,12 @@
+from lm_saes.sae import SparseAutoEncoder
+import torch
+
+
+@torch.no_grad()
+def merge_pre_enc_bias_to_enc_bias(sae: SparseAutoEncoder):
+	assert sae.cfg.apply_decoder_bias_to_pre_encoder
+
+	sae.cfg.apply_decoder_bias_to_pre_encoder = False
+	sae.encoder.bias.data = sae.encoder.bias.data - sae.encoder.weight.data @ sae.decoder.bias.data
+
+	return sae
