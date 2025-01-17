fix(topk activation): add keepdim=True to enable broadcasting; make dtype consistent without hardcode

src/lm_saes/trainer.py
@@ -7,11 +7,11 @@ import torch.optim.lr_scheduler as lr_scheduler
 from torch import Tensor
 from torch.optim import Adam, Optimizer
 from tqdm import tqdm
+from wandb.sdk.wandb_run import Run
 
 from lm_saes.config import TrainerConfig
 from lm_saes.optim import get_scheduler
 from lm_saes.sae import SparseAutoEncoder
-from wandb.sdk.wandb_run import Run
 
 
 class Trainer: