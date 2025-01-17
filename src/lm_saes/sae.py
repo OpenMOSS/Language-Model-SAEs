fix(topk activation): add keepdim=True to enable broadcasting; make d… (#73)
* fix(topk activation): add keepdim=True to enable broadcasting; make dtype consistent without hardcode

* fix(topk activation): add keepdim=True to enable broadcasting; make dtype consistent without hardcode

* fix(topk activation): add keepdim=True to enable broadcasting; make dtype consistent without hardcode

src/lm_saes/sae.py
@@ -92,31 +92,37 @@ class SparseAutoEncoder(HookedRootModule):
             return decoder_norm
 
     def activation_function_factory(self, cfg: BaseSAEConfig) -> Callable[[torch.Tensor], torch.Tensor]:  # type: ignore
-        assert cfg.act_fn.lower() in ["relu", "topk", "jumprelu", "batchtopk"], f"Not implemented activation function {cfg.act_fn}"
+        assert cfg.act_fn.lower() in [
+            "relu",
+            "topk",
+            "jumprelu",
+            "batchtopk",
+        ], f"Not implemented activation function {cfg.act_fn}"
         if cfg.act_fn.lower() == "relu":
-            return lambda x: x.gt(0).float()
+            return lambda x: x.gt(0).to(x.dtype)
         elif cfg.act_fn.lower() == "jumprelu":
-            return lambda x: x.gt(cfg.jump_relu_threshold).float()
+            return lambda x: x.gt(cfg.jump_relu_threshold).to(x.dtype)
         elif cfg.act_fn.lower() == "topk":
 
             def topk_activation(x: torch.Tensor):
                 x = torch.clamp(x, min=0.0)
                 k = x.shape[-1] - self.current_k + 1
-                k_th_value, _ = torch.kthvalue(x, k=k, dim=-1)
-                return x.ge(k_th_value).float()
+                k_th_value, _ = torch.kthvalue(x, k=k, dim=-1, keepdim=True)
+                return x.ge(k_th_value).to(x.dtype)
 
             return topk_activation
-        
+
         elif cfg.act_fn.lower() == "batchtopk":
+
             def topk_activation(x: torch.Tensor):
                 assert x.dim() == 2
                 batch_size = x.size(0)
-                
+
                 x = torch.clamp(x, min=0.0)
                 k = x.numel() - self.current_k * batch_size + 1
                 k_th_value, _ = torch.kthvalue(x.flatten(), k=k, dim=-1)
-                return x.ge(k_th_value).float()
-            
+                return x.ge(k_th_value).to(x.dtype)
+
             return topk_activation
 
     def compute_norm_factor(self, x: torch.Tensor, hook_point: str) -> torch.Tensor:  # type: ignore

tests/unit/test_sae.py
@@ -88,11 +88,28 @@ def test_set_norm(sae: SparseAutoEncoder):
 
 def test_sae_activate_fn(sae_config: SAEConfig, sae: SparseAutoEncoder):
     sae.current_k = 2
+    print(
+        sae.activation_function(
+            torch.tensor(
+                [[1.0, 2.0, 3.0, 4.0, 5.0, 6.0], [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]],
+                device=sae_config.device,
+                dtype=sae_config.dtype,
+            )
+        )
+    )
     assert torch.allclose(
         sae.activation_function(
-            torch.tensor([[1.0, 2.0, 3.0, 4.0, 5.0, 6.0]], device=sae_config.device, dtype=sae_config.dtype)
+            torch.tensor(
+                [[1.0, 2.0, 3.0, 4.0, 5.0, 6.0], [5.0, 6.0, 1.0, 2.0, 3.0, 4.0]],
+                device=sae_config.device,
+                dtype=sae_config.dtype,
+            )
         ).to(sae_config.device, sae_config.dtype),
-        torch.tensor([[0.0, 0.0, 0.0, 0.0, 1.0, 1.0]], device=sae_config.device, dtype=sae_config.dtype),
+        torch.tensor(
+            [[0.0, 0.0, 0.0, 0.0, 1.0, 1.0], [1.0, 1.0, 0.0, 0.0, 0.0, 0.0]],
+            device=sae_config.device,
+            dtype=sae_config.dtype,
+        ),
         atol=1e-4,
         rtol=1e-5,
     )