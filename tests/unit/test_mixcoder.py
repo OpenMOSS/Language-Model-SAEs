722ebf2345ee062bf149e32f817abe5001c7dcda
frankstein <frankstein73@outlook.com>
2025-01-23 14:30:37 +0800

    fix(mixcoder): fix topk activation func

--- tests/unit/test_mixcoder.py
+++ tests/unit/test_mixcoder.py
@@ -12,11 +12,13 @@ def config():
         modalities={"text": 2, "image": 3, "shared": 4},
         device="cpu",
         dtype=torch.float32,
-        use_glu_encoder=False,
+        use_glu_encoder=True,
         use_decoder_bias=True,
         hook_point_in="hook_point_in",
         hook_point_out="hook_point_out",
         expansion_factor=1.0,
+        top_k=2,
+        act_fn="topk",
     )
 
 
@@ -32,6 +34,12 @@ def modality_indices():
 def mixcoder(config, modality_indices):
     model = MixCoder(config)
     model.init_parameters(modality_indices=modality_indices)
+    model.decoder["text"].bias.data = torch.rand_like(model.decoder["text"].bias.data)
+    model.decoder["image"].bias.data = torch.rand_like(model.decoder["image"].bias.data)
+    model.decoder["shared"].bias.data = torch.rand_like(model.decoder["shared"].bias.data)
+    model.encoder["text"].bias.data = torch.rand_like(model.encoder["text"].bias.data)
+    model.encoder["image"].bias.data = torch.rand_like(model.encoder["image"].bias.data)
+    model.encoder["shared"].bias.data = torch.rand_like(model.encoder["shared"].bias.data)
     return model
 
 
@@ -80,6 +88,7 @@ def test_encode_decode(mixcoder, config):
         ),
         feature_acts[:, slice(*modality_index["shared"])],
     )
+    print(feature_acts)
 
     # Test decode
     reconstructed = mixcoder.decode(feature_acts)
@@ -95,18 +104,18 @@ def test_encode_decode(mixcoder, config):
     assert torch.allclose(reconstructed_image[4:, :], reconstructed[4:, :])
 
 
-def test_get_modality_activation(mixcoder, config):
+def test_get_modality_activation_mask(mixcoder, config):
     """Test the _get_modality_activation method."""
     batch_size = 8
     x = torch.ones(batch_size, config.d_model)
     tokens = torch.tensor([1, 2, 3, 4, 5, 6, 7, 8])
 
     # Test text modality
-    text_activation = mixcoder._get_modality_activation(x, tokens, "text")
-    assert torch.all(text_activation[0, :4] == 1)  # First 4 positions should be 1
-    assert torch.all(text_activation[0, 4:] == 0)  # Last 4 positions should be 0
+    text_activation_mask = mixcoder._get_modality_activation_mask(x, tokens, "text")
+    assert torch.all(text_activation_mask[0, :4] == 1)  # First 4 positions should be 1
+    assert torch.all(text_activation_mask[0, 4:] == 0)  # Last 4 positions should be 0
 
     # Test image modality
-    image_activation = mixcoder._get_modality_activation(x, tokens, "image")
-    assert torch.all(image_activation[1, :4] == 0)  # First 4 positions should be 0
-    assert torch.all(image_activation[1, 4:] == 1)  # Last 4 positions should be 1
+    image_activation_mask = mixcoder._get_modality_activation_mask(x, tokens, "image")
+    assert torch.all(image_activation_mask[1, :4] == 0)  # First 4 positions should be 0
+    assert torch.all(image_activation_mask[1, 4:] == 1)  # Last 4 positions should be 1
