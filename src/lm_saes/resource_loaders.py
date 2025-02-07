cfc9abff8bbce78caaf52c9c3f5d242683af6640
Dest1n1 <dest1nis2022@gmail.com>
2025-02-07 16:56:31 +0800

    feat(runner): support train with non-pre-generated activations

--- src/lm_saes/resource_loaders.py
+++ src/lm_saes/resource_loaders.py
@@ -36,7 +36,9 @@ def load_dataset(
     start_shard: int = 0,
 ) -> tuple[datasets.Dataset, Optional[dict[str, Any]]]:
     if not cfg.is_dataset_on_disk:
-        dataset = datasets.load_dataset(cfg.dataset_name_or_path, split="train", cache_dir=cfg.cache_dir)
+        dataset = datasets.load_dataset(
+            cfg.dataset_name_or_path, split="train", cache_dir=cfg.cache_dir, trust_remote_code=True
+        )
     else:
         dataset = datasets.load_from_disk(cfg.dataset_name_or_path)
     dataset = cast(datasets.Dataset, dataset)
