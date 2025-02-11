5050d6fd4d6b76cddfaa9aeb98a8bd3db03cbc0a
Hzfinfdu <zfhe00@gmail.com>
2025-02-11 16:43:39 +0800

    fix import errors introduced in merge

--- src/lm_saes/trainer.py
+++ src/lm_saes/trainer.py
@@ -10,9 +10,9 @@ from tqdm import tqdm
 from wandb.sdk.wandb_run import Run
 
 from lm_saes.config import TrainerConfig
+from lm_saes.mixcoder import MixCoder
 from lm_saes.optim import get_scheduler
 from lm_saes.sae import SparseAutoEncoder
-from lm_saes.mixcoder import MixCoder
 from lm_saes.utils.misc import all_reduce_tensor
 
 
