e455152a1401ea54405a3729d2eefa026e6f644a
Hzfinfdu <zfhe00@gmail.com>
2025-02-19 20:16:21 +0800

    fix(kernels): fix bugs in kernel tests

--- src/lm_saes/kernels.py
+++ src/lm_saes/kernels.py
@@ -550,7 +550,7 @@ if __name__ == "__main__":
         # Warmup runs (to eliminate startup overhead)
         for _ in range(warmup):
             torch_output = decoder(dense_input)
-            triton_output = decode_with_triton_spmm_kernel(dense_input, decoder.weight)
+            triton_output = decode_with_triton_spmm_kernel(dense_input, decoder.weight, require_precise_feature_acts_grad)
             assert isinstance(triton_output, torch.Tensor), "triton_output is not a torch.Tensor"
             grad_output = torch.randn_like(triton_output)
             triton_output.backward(grad_output)
@@ -578,7 +578,7 @@ if __name__ == "__main__":
 
         start_triton.record()  # type: ignore
         for _ in range(iters):
-            triton_output = decode_with_triton_spmm_kernel(dense_input, decoder.weight)
+            triton_output = decode_with_triton_spmm_kernel(dense_input, decoder.weight, require_precise_feature_acts_grad)
             assert isinstance(triton_output, torch.Tensor), "triton_output is not a torch.Tensor"
             grad_output = torch.randn_like(triton_output)
             triton_output.backward(grad_output)
