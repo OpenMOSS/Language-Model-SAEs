c38d6ab44a5dbdad360609ff72c108a3f90cbc8f
Dest1n1 <dest1nis2022@gmail.com>
2025-03-05 20:39:54 +0800

    feat(visualization): add modality specific info

--- src/lm_saes/database.py
+++ src/lm_saes/database.py
@@ -38,6 +38,8 @@ class FeatureAnalysis(BaseModel):
     name: str
     act_times: int
     max_feature_acts: float
+    act_times_modalities: Optional[dict[str, float]] = None
+    max_feature_acts_modalities: Optional[dict[str, float]] = None
     samplings: list[FeatureAnalysisSampling]
 
 
