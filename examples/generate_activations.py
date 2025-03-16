17d73ae6b3ecff471e6ed9e27572e8611f3cc6ed
Hzfinfdu <zfhe00@gmail.com>
2025-03-16 20:00:01 +0800

    update tutorial

--- examples/generate_activations.py
+++ examples/generate_activations.py
@@ -9,21 +9,24 @@ from lm_saes import (
 )
 
 if __name__ == "__main__":
+    layers = [0, 5]
+    
     settings = GenerateActivationsSettings(
         model=LanguageModelConfig(
-            model_name="EleutherAI/pythia-160m",
+            model_name="gpt2",
             device="cuda",
+            model_from_pretrained_path="path/to/gpt2",  # download https://huggingface.co/openai-community/gpt2
             dtype="torch.float32",
         ),
         model_name="pythia-160m",
         dataset=DatasetConfig(
-            dataset_name_or_path="Skylion007/openwebtext",
+            dataset_name_or_path="path/to/SlimPajama-3B",  # download https://huggingface.co/datasets/Hzfinfdu/SlimPajama-3B to your local disk.
             is_dataset_on_disk=True,
         ),
-        dataset_name="openwebtext",
-        hook_points=[f"blocks.{layer}.ln1.hook_normalized" for layer in range(12)],
+        dataset_name="SlimPajama-3B",
+        hook_points=[f"blocks.{layer}.hook_resid_post" for layer in layers],
         output_dir="activations",
-        total_tokens=800_000_000,
+        total_tokens=801_000_000,
         context_size=1024,
         n_samples_per_chunk=1,
         model_batch_size=32,
