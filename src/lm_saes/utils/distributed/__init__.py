2a762af689c169c8991598bb1565a6074ed4321f
Xuyang Ge <dest1nis2022@gmail.com>
2025-10-16 10:47:26 +0800

    fix(crosscoder): dp gradient should be partial

--- src/lm_saes/utils/distributed/__init__.py
+++ src/lm_saes/utils/distributed/__init__.py
@@ -1,5 +1,5 @@
 from .dimmap import DimMap
 from .ops import distributed_topk
-from .utils import all_gather_dict, mesh_dim_size, mesh_rank
+from .utils import all_gather_dict, mesh_dim_size, mesh_rank, replace_placements
 
-__all__ = ["DimMap", "distributed_topk", "all_gather_dict", "mesh_dim_size", "mesh_rank"]
+__all__ = ["DimMap", "distributed_topk", "all_gather_dict", "mesh_dim_size", "mesh_rank", "replace_placements"]
