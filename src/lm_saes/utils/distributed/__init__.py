253a7e0f77258b6d8909647324c967f6aefbcac7
frankstein <frankstein73@outlook.com>
2025-11-13 22:27:10 +0800

    feat(analyze): add functionality to save analysis results to file and update analysis with logits; enhance settings for output directory and feature analysis name

--- src/lm_saes/utils/distributed/__init__.py
+++ src/lm_saes/utils/distributed/__init__.py
@@ -1,6 +1,6 @@
 from .dimmap import DimMap
 from .ops import distributed_topk, full_tensor, masked_fill, to_local
-from .utils import all_gather_dict, mesh_dim_rank, mesh_dim_size, mesh_rank, replace_placements
+from .utils import all_gather_dict, all_gather_list, mesh_dim_rank, mesh_dim_size, mesh_rank, replace_placements
 
 __all__ = [
     "DimMap",
@@ -9,6 +9,7 @@ __all__ = [
     "to_local",
     "full_tensor",
     "all_gather_dict",
+    "all_gather_list",
     "mesh_dim_rank",
     "mesh_dim_size",
     "mesh_rank",
